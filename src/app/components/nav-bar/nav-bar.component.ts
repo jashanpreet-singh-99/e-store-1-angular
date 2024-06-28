b8fb1e627ef5fb719302ceecddfee34ece6f3a99
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-06-28 01:56:02 -0400

    ES-11: Added Mobile NavMenu

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,47 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+enum NavMode{
+  Open,
+  Closed
+}
 
 @Component({
   selector: 'nav-bar',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
+  animations: [
+    trigger('expandCollapse', [
+      state('void', style({
+        height: '0px',
+        opacity: 0
+      })),
+      state('*', style({
+        height: '*',
+        opacity: 1
+      })),
+      transition('void <=> *', [
+        animate('500ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class NavBarComponent {
 
+  state: NavMode = NavMode.Open;
+
+  toggleNavMode(): void {
+    this.state = (this.state === NavMode.Open) ? NavMode.Closed : NavMode.Open;
+  }
+
+  get NavMenuIcon(): string {
+    return (this.state === NavMode.Closed) ? 'menu' : 'close';
+  }
+
+  get showMobileMenu(): boolean {
+    return (this.state === NavMode.Open) ? true : false;
+  }
 }
