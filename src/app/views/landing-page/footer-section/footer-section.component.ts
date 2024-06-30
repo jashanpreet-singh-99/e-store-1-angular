491a7c6579863c0b343ccde7fb869bd75475da72
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-06-30 02:06:56 -0400

    ES-1: Migrated footer section to component and added mobile support

---
+++ src/app/views/landing-page/footer-section/footer-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-section.component.html',
+  styleUrl: './footer-section.component.scss'
+})
+export class FooterSectionComponent {
+
+}
--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -1,9 +1,5 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ProductItemComponent } from "../../components/product-item/product-item.component";
-import { ListingItemComponent } from "../../components/listing-item/listing-item.component";
-import { CarousalComponent } from "../../components/carousal/carousal.component";
+import { Component } from '@angular/core';
 import { HeroSectionV1Component } from "./hero-section-v1/hero-section-v1.component";
 import { ProductSectionComponent } from "./product-section/product-section.component";
 import { FeaturesSectionComponent } from "./features-section/features-section.component";
@@ -13,16 +9,15 @@ import { SubscribeSectionComponent } from "./subscribe-section/subscribe-section
 import { TrendingSectionComponent } from "./trending-section/trending-section.component";
 import { ReviewSectionComponent } from "./review-section/review-section.component";
 import { BrandSectionComponent } from "./brand-section/brand-section.component";
+import { FooterSectionComponent } from "./footer-section/footer-section.component";
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.scss',
-    imports: [CommonModule, HeroSectionV1Component, ProductSectionComponent, FeaturesSectionComponent, NewArrivalSectionComponent, FeaturedProductsSectionComponent, SubscribeSectionComponent, TrendingSectionComponent, ReviewSectionComponent, BrandSectionComponent]
+    imports: [CommonModule, HeroSectionV1Component, ProductSectionComponent, FeaturesSectionComponent, NewArrivalSectionComponent, FeaturedProductsSectionComponent, SubscribeSectionComponent, TrendingSectionComponent, ReviewSectionComponent, BrandSectionComponent, FooterSectionComponent]
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent{
   
-  ngOnInit(): void {
-  }
 }
